6185d111027d6a6ed03604d2699e18aa2d179645
BRMusta <BRMust@outlook.com>
2024-09-23 01:14:34 +0100

    Commit 2024-0004

--- main.py
+++ main.py
@@ -5,9 +5,31 @@
 ----------------
 """
 
-from taipy import Gui
+from taipy.gui import Gui
+import taipy.gui.builder as tgb
+from math import cos, exp
 
-Gui(page="# Getting started with *Taipy*").run() # use_reloader=True
+value = 10
+
+
+def compute_data(decay: int) -> list:
+    return [cos(i/6) * exp(-i*decay/600) for i in range(100)]
+
+
+def on_slider(state):
+    state.data = compute_data(state.value)
+
+
+with tgb.Page() as page:
+    tgb.text(value="Taipy Getting Started", class_name="h1")
+    tgb.text(value="Value: {value}")
+    tgb.slider(value="{value}", on_change=on_slider)
+    tgb.chart(data="{data}")
+
+data = compute_data(value)
+
+if __name__ == "__main__":
+    Gui(page=page).run(title="Dynamic chart")
 
 
 """
